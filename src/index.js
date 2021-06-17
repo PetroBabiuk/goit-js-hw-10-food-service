95fc9f664cf972e11f2a26d582b6ee14acab4844
Petro Babiuk <75635325+PetroBabiuk@users.noreply.github.com>
2021-06-17 18:28:57 +0300

    добавляет файлы и разметку согласно ТЗ

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
 import './sass/main.scss';
+import templateItems from './templates/main.hbs';
+import menu from './menu.json';
+
+const menuRef = document.querySelector('.js-menu');
+const markup = templateItems(menu);
+
+menuRef.insertAdjacentHTML('beforeend', markup);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const checkbox = document.querySelector('.theme-switch__toggle');
+const STORAGE_KEY = 'currentTheme';
+const savedTheme = localStorage.getItem(STORAGE_KEY);
+
+document.body.classList.add(savedTheme);
+
+checkbox.addEventListener('change', onThemeChange);
+
+function onThemeChange() {
+    if (checkbox.checked) {
+        document.body.classList.replace(Theme.LIGHT, Theme.DARK);
+        localStorage.setItem(STORAGE_KEY, Theme.DARK);
+    } else {
+        document.body.classList.replace(Theme.DARK, Theme.LIGHT);
+    localStorage.setItem(STORAGE_KEY, Theme.LIGHT);
+    }
+    }
+        
+function ifLocalStorageHasDarkTheme() {
+  if (savedTheme === Theme.DARK) {
+    document.body.classList.replace(Theme.LIGHT, Theme.DARK);
+    checkbox.setAttribute('checked', true);
+  }
+}
+
+ifLocalStorageHasDarkTheme();
